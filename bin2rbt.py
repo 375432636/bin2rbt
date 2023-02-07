9ca3810d31b6abd521e464f30a42480d2c008677
John <375432636@qq.com>
2023-02-07 21:01:45 +0800

    添加注释和时间

--- bin2rbt.py
+++ bin2rbt.py
@@ -6,43 +6,39 @@ import click
 
 
 head_line = "Lattice Semiconductor Corporation ASCII Bitstream\r\n"
-gen_time  = "Date:"+"\r\n"*6
+gen_time  = "Date:"+f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"+"\r\n"*6
 
-endline1 = ""
-endline2 = ""
-
-def int2bin(int_number):
-    return "{0:08b}".format(int_number)
 
 def int2bin_list(int_number_list):
+    # convert the binary code to binary text one by one
     return "".join(["{0:08b}".format(i) for i in int_number_list])
 
 @click.command()
 @click.option('--binfile', default='binfile.bin', help='binfile.bin')
 @click.option('--rbtfile', default='rbtfile.txt', help='rbtfile.txt.')
 def bin2rbt(binfile, rbtfile):
-    binlines =   ""
+    # read file 
     with open(binfile, "rb") as bf:
         bin_data = bf.read()
-        for i in range(len(bin_data)-6):
-            if i%10 == 0:
-                binlines = binlines + "\r\n"
-            binlines += int2bin(bin_data[i])
-            
-        endline1 = "\r\n"+int2bin_list(bin_data[-6:-4]) + "\r\n"
-        endline2 = int2bin_list(bin_data[-4:])+"\r\n"
-        for i in range(6):
-            print(bin_data[(i-6)])
-        print( endline1)
-        print( endline2)
+
+    # process text
+    # convert the binary code to binary text every 10 byte. 
+    binlines = ""
+    for i in range(len(bin_data)-6):
+        if i%10 == 0:
+            binlines = binlines + "\r\n"
+        binlines += int2bin_list([bin_data[i]])
+    binlines+= "\r\n"
     
+    # the laste 6 byte need specific format
+    endline1 = int2bin_list(bin_data[-6:-4]) + "\r\n"
+    endline2 = int2bin_list(bin_data[-4:])+"\r\n"
+
+    # save the text to file 
     with open(rbtfile, "wb") as rf:
-        context_lines = []
-        context_lines.append(head_line)
-        context_lines.append(gen_time)
-        context_lines.append(binlines)
-        context_lines.append(endline1)
-        context_lines.append(endline2)
+        context_lines = head_line + \
+                gen_time + binlines + \
+                endline1 + endline2 
         context_lines = "".join(context_lines).encode()
         rf.write(context_lines)
 
